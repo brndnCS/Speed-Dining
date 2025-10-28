33fc4dab8ce32f6abc01156a18806f0de44e634a
Sparsh Pandey <sparshpandey@Splashs-MacBook-Air.local>
2025-10-28 12:13:59 -0400

    Mern initial Commit

---
+++ backend/server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://sparshpandey06_db_user:q56faVzHcdrE9a0F@cluster0.ody1sc1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const client = new MongoClient(url);
+client.connect();
+
+
+app.use(cors());
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use((req, res, next) => {
+    app.get("/api/ping", (req, res, next) => {
+        res.status(200).json({ message: "Hello World" });
+    });
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.post('/api/addcard', async (req, res, next) => {
+    // incoming: userId, color
+    // outgoing: error
+    const { userId, card } = req.body;
+    const newCard = { Card: card, UserId: userId };
+    var error = '';
+    try {
+        const db = client.db('COP4331Cards');
+        const result = db.collection('Cards').insertOne(newCard);
+    }
+    catch (e) {
+        error = e.toString();
+    }
+    cardList.push(card);
+    var ret = { error: error };
+    res.status(200).json(ret);
+});
+
+
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { login, password } = req.body;
+    var id = -1;
+    var fn = '';
+    var ln = '';
+
+    const db = client.db('COP4331');
+    const results = await db.collection('users').find({ Login: login, Password: password }).toArray();
+
+    var id = -1;
+    var fn = '';
+    var ln = '';
+
+
+    if (results.length > 0) {
+        id = results[0].UserID;
+        fn = results[0].fName;
+        ln = results[0].lName;
+    }
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
+});
+app.post('/api/searchcards', async (req, res, next) => {
+    // incoming: userId, search
+    // outgoing: results[], error
+    var error = '';
+    const { userId, search } = req.body;
+    var _search = search.trim();
+    const db = client.db('COP4331Cards');
+    const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'i' } }).toArray();
+    var _ret = [];
+    for (var i = 0; i < results.length; i++) {
+        _ret.push(results[i].Card);
+    }
+    var ret = { results: _ret, error: error };
+    res.status(200).json(ret);
+});
+
+app.listen(5001); // start Node + Express server on port 5000
