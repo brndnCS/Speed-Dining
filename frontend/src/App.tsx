dddede76b05a07d662d0a6491cfde127d6cd70eb
David L <davidlw0107@gmail.com>
2025-11-11 17:05:00 -0500

    Added loading page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,15 @@ import './App.css';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CardPage from './pages/CardPage';
+import LoadingPageWrapper from './pages/LoadingPage';
+
 function App() {
     return (
         <Router >
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<SignupPage />} />
+                <Route path="/loading" element={<LoadingPageWrapper />} />
                 <Route path="/cards" element={<CardPage />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
---
+++ frontend/src/pages/LoadingPage.tsx
@@ -0,0 +1,7 @@
+import LoadingPage from '../components/LoadingPage';
+
+const LoadingPageWrapper = () => {
+  return <LoadingPage />;
+};
+
+export default LoadingPageWrapper;
